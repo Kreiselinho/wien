b1249fbb85a8d3ba2262d80c2b3f894083e31f59
Kreiselinho <dominic.kreisel@student.uibk.ac.at>
2024-04-26 16:50:34 +0200

    Überflüssige Zeilen gelöscht

--- main.js
+++ main.js
@@ -132,15 +132,6 @@ async function loadlines(url) {
   }).addTo(themaLayer.lines);
 }
 loadlines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json")
-
-/* 
-bus_1.png Red
-bus_2.png Yellow
-bus_3.png Blue
-bus_4.png Green
-bus_5.png Grey
-bus_6.png Orange
-*/
 async function loadstops(url) {
   console.log("Loading", url);
   let response = await fetch(url);
